de5db49787eb67489efb71e0e04060ac7b89ed52
wak205 <pranandaariq@gmail.com>
2021-05-13 13:36:02 +0700

    Android Studio Telat Malam 1

---
+++ app/src/main/java/com/example/capstoneproject/SplashScreenActivity.kt
@@ -0,0 +1,29 @@
+package com.example.capstoneproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var handler: Handler
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+
+        handler = Handler(mainLooper)
+        handler.postDelayed({
+            toMain()
+        }, 3000)
+    }
+    private fun toMain(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+}
